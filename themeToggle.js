6f02385a3755ddfaeebd07118da6c9a554be4834
Nick <91295294+nickadesina@users.noreply.github.com>
2023-10-30 04:37:13 -0700

    Update themeToggle.js
    remember preference

--- themeToggle.js
+++ themeToggle.js
@@ -1,12 +1,26 @@
-document.getElementById('toggleTheme').addEventListener('click', function() {
-    let body = document.body;
-    let themeIcon = document.getElementById('themeIcon');
-    
-    if (body.classList.contains('dark-mode')) {
-        body.classList.remove('dark-mode');
-        themeIcon.src = "images/moon-solid.svg";  // Moon image representing light mode
-    } else {
-        body.classList.add('dark-mode');
-        themeIcon.src = "images/sun-solid.svg";  // Sun image representing dark mode
+document.addEventListener('DOMContentLoaded', function() {
+    const body = document.body;
+    const themeToggleButton = document.getElementById('toggleTheme');
+    const themeIcon = document.getElementById('themeIcon');
+
+    // Function to toggle the theme
+    function toggleTheme() {
+        body.classList.toggle('dark-mode');
+        if (body.classList.contains('dark-mode')) {
+            themeIcon.src = 'images/sun-solid.svg'; // Optional: Update to a sun icon for dark mode
+            localStorage.setItem('theme', 'dark');
+        } else {
+            themeIcon.src = 'images/moon-solid.svg'; // Reset back to moon icon for light mode
+            localStorage.setItem('theme', 'light');
+        }
     }
+
+    // Check local storage and apply user's theme preference
+    if (localStorage.getItem('theme') === 'dark') {
+        body.classList.add('dark-mode');
+        themeIcon.src = 'images/sun-solid.svg'; // Optional: Update to a sun icon for dark mode
+    }
+
+    // Event listener for the button
+    themeToggleButton.addEventListener('click', toggleTheme);
 });
